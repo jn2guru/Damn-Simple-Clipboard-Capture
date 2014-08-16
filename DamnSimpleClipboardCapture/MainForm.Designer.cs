d60465168ed6483fe22f40fa2a7c75e110a98308
Jordan McGuigan <jordanmcguigan@gmail.com>
2014-08-15 22:24:42 -0400

    Initial commit of working project

---
+++ DamnSimpleClipboardCapture/MainForm.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace DamnSimpleClipboardCapture
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.WriteToFileButton = new System.Windows.Forms.Button();
+            this.LogTextBox = new System.Windows.Forms.TextBox();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.clipboardMonitor1 = new DamnSimpleClipboardCapture.ClipboardMonitor();
+            this.AutoSaveCheckbox = new System.Windows.Forms.CheckBox();
+            this.UpdatePathFolderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PathTextBox = new System.Windows.Forms.TextBox();
+            this.RunningLogTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // WriteToFileButton
+            // 
+            this.WriteToFileButton.Location = new System.Drawing.Point(322, 255);
+            this.WriteToFileButton.Name = "WriteToFileButton";
+            this.WriteToFileButton.Size = new System.Drawing.Size(193, 23);
+            this.WriteToFileButton.TabIndex = 1;
+            this.WriteToFileButton.Text = "Write To File";
+            this.WriteToFileButton.UseVisualStyleBackColor = true;
+            this.WriteToFileButton.Click += new System.EventHandler(this.WriteToFileButton_Click);
+            // 
+            // LogTextBox
+            // 
+            this.LogTextBox.Location = new System.Drawing.Point(12, 12);
+            this.LogTextBox.MaxLength = 100000;
+            this.LogTextBox.Multiline = true;
+            this.LogTextBox.Name = "LogTextBox";
+            this.LogTextBox.ReadOnly = true;
+            this.LogTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.LogTextBox.Size = new System.Drawing.Size(632, 202);
+            this.LogTextBox.TabIndex = 2;
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(138, 255);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(178, 23);
+            this.ClearButton.TabIndex = 3;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // clipboardMonitor1
+            // 
+            this.clipboardMonitor1.BackColor = System.Drawing.Color.Red;
+            this.clipboardMonitor1.Location = new System.Drawing.Point(12, 12);
+            this.clipboardMonitor1.Name = "clipboardMonitor1";
+            this.clipboardMonitor1.Size = new System.Drawing.Size(75, 23);
+            this.clipboardMonitor1.TabIndex = 0;
+            this.clipboardMonitor1.Text = "MainClipboardMonitor";
+            this.clipboardMonitor1.Visible = false;
+            this.clipboardMonitor1.ClipboardChanged += new System.EventHandler<DamnSimpleClipboardCapture.ClipboardChangedEventArgs>(this.MainClipboardMonitor_ClipboardChanged);
+            // 
+            // AutoSaveCheckbox
+            // 
+            this.AutoSaveCheckbox.AutoSize = true;
+            this.AutoSaveCheckbox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.AutoSaveCheckbox.Location = new System.Drawing.Point(531, 243);
+            this.AutoSaveCheckbox.Name = "AutoSaveCheckbox";
+            this.AutoSaveCheckbox.Size = new System.Drawing.Size(76, 17);
+            this.AutoSaveCheckbox.TabIndex = 4;
+            this.AutoSaveCheckbox.Text = "Auto Save";
+            this.AutoSaveCheckbox.UseVisualStyleBackColor = true;
+            this.AutoSaveCheckbox.CheckedChanged += new System.EventHandler(this.AutoSaveCheckbox_CheckedChanged);
+            // 
+            // UpdatePathFolderBrowserDialog
+            // 
+            this.UpdatePathFolderBrowserDialog.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(440, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "UpdatePathButton";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.UpdatePathButton_Click);
+            // 
+            // PathTextBox
+            // 
+            this.PathTextBox.Location = new System.Drawing.Point(138, 226);
+            this.PathTextBox.Name = "PathTextBox";
+            this.PathTextBox.ReadOnly = true;
+            this.PathTextBox.Size = new System.Drawing.Size(296, 20);
+            this.PathTextBox.TabIndex = 7;
+            // 
+            // RunningLogTimer
+            // 
+            this.RunningLogTimer.Enabled = true;
+            this.RunningLogTimer.Interval = 60000;
+            this.RunningLogTimer.Tick += new System.EventHandler(this.RunningLogTimer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 287);
+            this.Controls.Add(this.PathTextBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AutoSaveCheckbox);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.LogTextBox);
+            this.Controls.Add(this.WriteToFileButton);
+            this.Controls.Add(this.clipboardMonitor1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ClipboardMonitor clipboardMonitor1;
+        private System.Windows.Forms.Button WriteToFileButton;
+        private System.Windows.Forms.TextBox LogTextBox;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.CheckBox AutoSaveCheckbox;
+        private System.Windows.Forms.FolderBrowserDialog UpdatePathFolderBrowserDialog;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox PathTextBox;
+        private System.Windows.Forms.Timer RunningLogTimer;
+    }
+}
+
---
+++ DamnSimpleClipboardCapture/MainForm.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DamnSimpleClipboardCapture
+{
+    public partial class MainForm : Form
+    {
+        private bool AutoSaveEnabled = false;
+        private string basePath = string.Empty;
+        private string ClipboardLog = string.Empty;
+        private string RunningLog = string.Empty; 
+
+        public MainForm()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string basepath = Properties.Settings.Default["basePath"].ToString();
+            bool useDesktop = bool.Parse(Properties.Settings.Default["useDesktop"].ToString());
+            InitializeComponent();
+            SetPath(path, basepath, useDesktop);
+        }
+
+        private void SetPath(string path, string basepath = "", bool useDesktop = false, bool updateConfig = false)
+        {
+            if (useDesktop)
+            {
+                path = path + "\\" + basepath;
+            }
+            else if(string.IsNullOrEmpty(basepath))
+            {
+                //do nothing (for now)
+            }
+            else
+            {
+                path = basepath;
+            }
+            //set the final base path
+            this.basePath = path;
+            PathTextBox.Text = this.basePath;
+
+            if(updateConfig)
+            {
+                Properties.Settings.Default["basePath"] = this.basePath;
+                Properties.Settings.Default["useDesktop"] = false;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void WriteToLog(string message)
+        {
+            ClipboardLog += message;
+            RunningLog += message;
+            LogTextBox.Text = ClipboardLog;
+            if(AutoSaveEnabled)
+            {
+                string path = string.Format("{0}\\autosave_{1}.txt", this.basePath, DateTime.Now.ToString("yyyy-MM-ddTHH-mm-ss"));
+                File.WriteAllText(path, ClipboardLog);
+            }
+        }
+
+        private void ClearLog()
+        {
+            ClipboardLog = string.Empty;
+            RunningLog = string.Empty;
+            LogTextBox.Text = ClipboardLog;
+        }
+        private void WriteToFileButton_Click(object sender, EventArgs e)
+        {
+            string path = string.Format("{0}\\clipboard_log_{1}.txt", this.basePath, DateTime.Now.ToString("yyyy-MM-ddTHH-mm-ss"));
+            File.WriteAllText(path, ClipboardLog);
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            ClearLog();
+        }
+
+        private void MainClipboardMonitor_ClipboardChanged(object sender, ClipboardChangedEventArgs e)
+        {
+            string clipboardText = string.Empty;
+
+            try
+            {
+                clipboardText = e.DataObject.GetData(System.Windows.Forms.DataFormats.UnicodeText).ToString();
+            }
+            catch (Exception ex)
+            {
+                clipboardText = "Exception Occured while capturing clipboard: " + ex.ToString();
+            }
+            finally
+            {
+                WriteToLog(clipboardText);
+            }
+           
+        }
+
+        private void AutoSaveCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            AutoSaveEnabled = AutoSaveCheckbox.Checked;
+        }
+
+        private void UpdatePathButton_Click(object sender, EventArgs e)
+        {
+            //SetPath(PathTextBox.Text);
+            DialogResult result = UpdatePathFolderBrowserDialog.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                SetPath(UpdatePathFolderBrowserDialog.SelectedPath, updateConfig:true);
+            }
+        }
+
+
+
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RunningLogTimer_Tick(object sender, EventArgs e)
+        {
+            string path = string.Format("{0}\\running_log_{1}.txt", this.basePath, DateTime.Now.ToString("yyyy-MM-ddTHH-mm-ss"));
+            File.WriteAllText(path, RunningLog);
+            RunningLog = string.Empty;
+        }
+    }
+}
